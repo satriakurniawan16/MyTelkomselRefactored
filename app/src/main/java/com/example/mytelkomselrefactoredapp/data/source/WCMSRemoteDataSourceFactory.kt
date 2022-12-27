f39bed32923767bf5658800871a0fb604fd2e04e
RahmadSatria <satriakurniaw4n@gmail.com>
2022-12-27 10:06:21 +0700

    clean gradle

--- app/src/main/java/com/example/mytelkomselrefactoredapp/data/source/WCMSRemoteDataSourceFactory.kt
+++ app/src/main/java/com/example/mytelkomselrefactoredapp/data/source/WCMSRemoteDataSourceFactory.kt
@@ -3,8 +3,19 @@ package com.example.mytelkomselrefactoredapp.data.source
 import javax.inject.Inject
 
 open class WCMSRemoteDataSourceFactory @Inject constructor(
-    private val remoteDataSource: WCMSRemoteDataSource
+    private val remoteDataSource: WCMSRemoteDataSource,
+    // local datasource resource
 ) {
+
+//    if(cached){
+//        get Local
+//    }else{
+//        get Remote
+//    }
+
+
+//    fun getLocalDatasource
+
     fun getRemoteDataSource(): WCMSRemoteDataSource{
         return remoteDataSource
     }
